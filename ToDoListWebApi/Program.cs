decbc71204f72b6b47718a3997a439c37b292ab6
max.shatilo@gmail.com <max.shatilo@gmail.com>
2023-01-21 15:02:01 +0200

    Add swagger

--- ToDoListWebApi/Program.cs
+++ ToDoListWebApi/Program.cs
@@ -4,10 +4,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.MapControllers();
+
+app.UseSwagger();
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.Run();
