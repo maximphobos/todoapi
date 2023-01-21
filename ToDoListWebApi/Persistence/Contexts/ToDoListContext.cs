4d932c272025ed52446158b5e8328a5883f832d7
max.shatilo@gmail.com <max.shatilo@gmail.com>
2023-01-21 15:15:32 +0200

    Add DbContext

---
+++ ToDoListWebApi/Persistence/Contexts/ToDoListContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoListWebApi.Persistence.Models;
+
+namespace ToDoListWebApi.Persistence.Contexts
+{
+    public class ToDoListContext : DbContext
+    {
+        public ToDoListContext(DbContextOptions<ToDoListContext> options)
+        : base(options)
+        {
+        }
+
+        public virtual DbSet<ToDoTask>? ToDoTasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ ToDoListWebApi/Persistence/Models/ToDoTask.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoListWebApi.Persistence.Models
+{
+    public class ToDoTask
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string? TaskBodyText { get; set; }
+    }
+}
