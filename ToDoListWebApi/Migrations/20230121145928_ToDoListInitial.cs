52cff22c421ace754a9ff3a2aafdbb5fe8629f27
max.shatilo@gmail.com <max.shatilo@gmail.com>
2023-01-21 17:06:48 +0200

    Add ToDoListInitial migration

---
+++ ToDoListWebApi/Migrations/20230121145928_ToDoListInitial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoListWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ToDoListInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDoTasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TaskBodyText = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoTasks", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoTasks");
+        }
+    }
+}
