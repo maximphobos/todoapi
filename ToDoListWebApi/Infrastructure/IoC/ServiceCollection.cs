0297d238e61bd3da4e4be49554513b09bfef382a
max.shatilo@gmail.com <max.shatilo@gmail.com>
2023-01-23 20:07:54 +0200

    Move services registration to the different class

---
+++ ToDoListWebApi/Infrastructure/IoC/ServiceCollection.cs
@@ -0,0 +1,68 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Reflection;
+using ToDoListWebApi.Infrastructure.Mapping;
+using ToDoListWebApi.Persistence.Contexts;
+using ToDoListWebApi.Persistence.Repositories;
+using ToDoListWebApi.Services;
+
+namespace ToDoListWebApi.Infrastructure.IoC
+{
+    public static class ServiceCollection
+    {
+        public static IServiceCollection AddServices (this IServiceCollection services) 
+        {
+            services.AddControllers();
+            services.AddScoped<IToDoListService, ToDoListService>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddFluentValidationServices(this IServiceCollection services)
+        {
+            services.AddFluentValidationAutoValidation();
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+
+        public static IServiceCollection AddSwaggerServices(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c => c.AddFluentValidationRulesScoped());
+            services.AddFluentValidationRulesToSwagger();
+
+            return services;
+        }
+
+        public static IServiceCollection AddLoggerServices(this IServiceCollection services)
+        {
+            services.AddSingleton(sp => sp.GetRequiredService<ILoggerFactory>().CreateLogger("DefaultLogger"));
+
+            return services;
+        }
+
+        public static IServiceCollection AddDbContextServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ToDoListContext>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddRepositoryServices(this IServiceCollection services)
+        {
+            services.AddTransient<IToDoListRepository, ToDoListRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(MapperProfile));
+            services.AddScoped<IModelMapper, ModelMapper>();
+
+            return services;
+        }
+    }
+}
--- ToDoListWebApi/Program.cs
+++ ToDoListWebApi/Program.cs
@@ -1,40 +1,14 @@
-using FluentValidation;
-using FluentValidation.AspNetCore;
-using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
-using Swashbuckle.AspNetCore.Swagger;
-using System.Reflection;
-using ToDoListWebApi.Infrastructure.Mapping;
-using ToDoListWebApi.Persistence.Contexts;
-using ToDoListWebApi.Persistence.Repositories;
-using ToDoListWebApi.Services;
-using ToDoListWebApi.ViewModels.ToDoListViewModels;
+using ToDoListWebApi.Infrastructure.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
-
-//Fluent validation
-builder.Services.AddFluentValidationAutoValidation();
-builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-
-builder.Services.AddSwaggerGen(c => c.AddFluentValidationRulesScoped());
-
-builder.Services.AddFluentValidationRulesToSwagger();
-
-//ILogger
-builder.Services.AddSingleton(sp => sp.GetRequiredService<ILoggerFactory>().CreateLogger("DefaultLogger"));
-
-// DbContext and Repositories
-builder.Services.AddDbContext<ToDoListContext>();
-builder.Services.AddTransient<IToDoListRepository, ToDoListRepository>();
-
-//Register custom services
-builder.Services.AddScoped<IToDoListService, ToDoListService>();
-
-//Infrastructure
-builder.Services.AddAutoMapper(typeof(MapperProfile));
-builder.Services.AddScoped<IModelMapper, ModelMapper>();
+builder.Services.AddServices()
+.AddFluentValidationServices()
+.AddSwaggerServices()
+.AddLoggerServices()
+.AddDbContextServices()
+.AddRepositoryServices()
+.AddInfrastructureServices();
 
 var app = builder.Build();
 
